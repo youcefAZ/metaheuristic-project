7c0d7b821a3c9b14196c224942795b54a7970d2b
Amiraa-m <78305244+Amiraa-m@users.noreply.github.com>
2021-05-20 19:38:20 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -35,6 +35,9 @@ public class Main {
         BFS bfs= new BFS(data,Integer.parseInt(parameters[2]),20);
         //bfs.printMatrix();
         int[]result=bfs.rechercheEnLargeur();
-
+        
+        DFS dfs= new DFS(data,Integer.parseInt(parameters[2]),20);
+        int [] DFSResult = dfs.rechercheEnProfondeur();
+        
     }
 }
